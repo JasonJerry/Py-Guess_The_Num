6d31f43e468a474297dceb19cd395de55322c717
JasonJerry <jasonjerry2105@protonmail.com>
2020-04-14 20:05:10 +0530

    This is the src code of the program

---
+++ game.py
@@ -0,0 +1,32 @@
+
+from random import randint
+
+computer = randint(0, 10)
+
+print('Hi! I just thought on a number beteween 0 and 10. Can you guess what number did i think?')
+
+hit = False
+
+guess = 0
+
+while not hit:
+
+    player = int(input('What is yout guess? '))
+
+    guess += 1
+
+    if player == computer:
+
+        hit = True
+
+    else:
+
+        if player < computer:
+
+            print('konjam mela(UP)')
+
+        elif player > computer:
+
+            print('konjam keela(DOWN)')
+
+print(' You did it! Found the right answer with {} attempts'.format(guess))
